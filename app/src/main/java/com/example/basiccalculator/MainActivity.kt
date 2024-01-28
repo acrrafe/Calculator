b8d12618a534d10a02f35f4e6aa5d2a8d015cf5c
Aron Rafe <lnsapollo0@gmail.com>
2024-01-29 00:36:28 +0800

    Add a Test Comment

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
+
+
+// THIS IS A CALCULATOR APPLICATION WITH BASIC FUNCTIONALITIES
+
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
     // Numbers
